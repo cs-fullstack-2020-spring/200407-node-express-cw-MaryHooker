32c0302e6e0f597a67d0b7c124887cd8f76963b0
MaryHooker <mc020791@yahoo.com>
2020-04-07 13:48:42 -0500

    Root Path Created & Tested

--- index.js
+++ index.js
@@ -2,9 +2,16 @@
 
 //Reference to the express module
 let express = require('express');
+
 //Create a server and save it in app
 let app = express();
+
 //Call the listen method on app/place a port and function as parameters/sanity in console
-app.listen(3000,(req,res)=>{
-    console.log("Listening on port 3000")
+app.listen(6000,(req,res)=>{
+    console.log("Listening on port 6000")
+})
+
+//Define Root Path
+app.get('/',(req,res)=>{
+    res.send("Welcome to my node & express server");
 })
