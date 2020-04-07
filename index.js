e62b48d309444ae96e2911568056cad2bfc7c85a
MaryHooker <mc020791@yahoo.com>
2020-04-07 13:41:05 -0500

    Reference express module/Created server/used listen method

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 //20-04-07 Node and Express CW
 
+//Reference to the express module
+let express = require('express');
+//Create a server and save it in app
+let app = express();
+//Call the listen method on app/place a port and function as parameters/sanity in console
+app.listen(3000,(req,res)=>{
+    console.log("Listening on port 3000")
+})
