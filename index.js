11d9cec04c249e35f8998f7c710bf9eec3a3e4c7
MaryHooker <mc020791@yahoo.com>
2020-04-07 21:13:38 -0500

    Iterated through array using forEach/Route to display

--- index.js
+++ index.js
@@ -10,43 +10,48 @@ let app = express();
 let name = 'Mary';
 
 //Define an array of student objects with 2 props
-let studentArray = [{name:'Mary',average: 80},{name:'Charlie',average:60},{name:'Lucy',average:100}]
+let studentArray = [{ name: 'Mary', average: 'High' }, { name: 'Charlie', average: 'Medium' }, { name: 'Lucy', average: 'Low' }];
 
 //Define Root Path
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("Welcome to my node & express server");
 })
 
 //Route to display name
-app.get('/displayName',(req,res)=>{
+app.get('/displayName', (req, res) => {
     res.send(`My name is ${name}`);
 })
 
 //Route to display student by index position
-app.get('/student/:id',(req,res)=>{
+app.get('/student/:id', (req, res) => {
     //function to render student in index position
-    studentArray.forEach((student,index)=>{
-        if(index == req.params.id){
-            res.send(`The student whose index position matches is ${student.name} and their avaerage grade is ${student.average}%`); 
-        }
-    })
+    studentArray.forEach((student) => {
+
+        let students =
+            (`The student whose index position matches is ${student.name} and their confidence is ${student.average}%`);
+    }
+    )
+
+    res.send(students)
 })
 
 //Route and function to display all students from array
-app.get('/allStudents',(req,res)=>{
-    studentArray.map((student,index)=>{
-        // if(index == 0,1,2){
-            res.send(`Student: ${student.name}, Average: ${student.average}%`);
-        // } else if(index == 1){
-        //     res.send(`Student: ${student.name}, Average: ${student.average}%`);
-        // } else if(index == 2){
-        //     res.send(`Student: ${student.name}, Average: ${student.average}%`);
+app.get('/allStudents', (req, res) => {
+
+    let newArray = '';
+    studentArray.forEach((student) => {
         
-     } )})
-// })
-// })
+            newArray = newArray + (`<h2>Student: ${student.name}, Confidence: ${student.average}</h2><hr>`);
+            console.log(newArray);
+        
+    })
+
+    console.log('end');
+
+    res.send( `${newArray}`);
+})
 
 //Call the listen method on app/place a port and function as parameters/sanity in console
-app.listen(7000,(req,res)=>{
+app.listen(7000, () => {
     console.log("Listening on port 7000")
 })
